918239eabb05f3409010c735da37859aa72d0169
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2022-07-28 18:19:45 +0900

    Add Vec and Waypoint classes.

---
+++ frenetic/executors/bicycleexecutor/vec.py
@@ -0,0 +1,8 @@
+class Vec:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def update(self, x, y):
+        self.x = x
+        self.y = y
---
+++ frenetic/executors/bicycleexecutor/waypoint.py
@@ -0,0 +1,9 @@
+import vec
+
+class WaypointTransform:
+    def __init__(self, location):
+        self.location = location
+
+class Waypoint:
+    def __init__(self, x, y):
+        self.transform = WaypointTransform(vec.Vec(x, y))
